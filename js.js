8fe9c1d7a68fdd999a60da0e4d9689fb1241ca7b
White-br0 <daymansolvatore88@gmail.com>
2024-11-11 17:20:38 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+function sayHello() {
+  alert("Привет, Андрей");
+}
+sayHello();
+
+let name = Анна;
+function sayHello('name') {
+    alert('Привет, ' + 'name'); // поэтому на выходе получится "Привет, Анна"
+  }
